d6981257e321fc3792739aa5d537328b915fcb5a
Kris Pritchard <krisrp@users.noreply.github.com>
2020-04-30 11:16:01 +1200

    Switch back to old db name

--- Assignment1/settings.py
+++ Assignment1/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'Assignment1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3.old'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
